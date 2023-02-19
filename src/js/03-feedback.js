f94b7a67e628136842307343fc0a805c7faf5f47
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-19 23:21:28 +0200

    add feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onMsgInput);
+
+function onMsgInput(e) {
+  const msg = e.currentTarget.value;
+
+  localStorage.setItem('message', msg);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+}
