071c7ab113e39a70e8d10084537c95280304b170
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-19 20:04:07 +0200

    add gallery script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>`;
+});
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
