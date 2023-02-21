055b9126dbe112a98e074c7916699b2c963e83a6
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-21 23:56:30 +0200

    add video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_TIME = 'videoplayer-current-time';
+
+const timeSum = localStorage.getItem(KEY_TIME);
+if (timeSum !== null) {
+  player.setCurrentTime(timeSum);
+}
+
+function timeUpdate(e) {
+  localStorage.setItem(KEY_TIME, e.seconds);
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
