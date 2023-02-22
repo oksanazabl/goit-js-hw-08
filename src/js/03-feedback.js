c8a3082c686860249227866037d387cd965efd7d
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-22 23:14:02 +0200

    fix feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,31 @@
+import throttle from 'lodash.throttle';
 import '../css/common.css';
 import '../css/03-feedback.css';
-import throttle from 'lodash.throttle';
+
+const SAVE_KEY = 'feedback-form-state';
+const formData = {};
 
 const form = document.querySelector('.feedback-form');
-const SAVE_KEY = 'feedback-form-state';
-const formObj = {};
-const parseData = JSON.parse(localStorage.getItem(SAVE_KEY, JSON.stringify(formObj)));
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onMsgInput, 500));
-form.addEventListener('input', e => {
-  formObj[e.target.name] = e.target.value;
-  onMsgInput();
-});
 
 populateTextarea();
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log(formObj);
+  console.log(formData);
 
   e.currentTarget.reset();
   localStorage.removeItem(SAVE_KEY);
 }
 
 function onMsgInput(e) {
-  return localStorage.setItem(SAVE_KEY, JSON.stringify(formObj));
+  formData[e.target.name] = e.target.value;
+  return localStorage.setItem(SAVE_KEY, JSON.stringify(formData));
 }
+
+const parseData = JSON.parse(localStorage.getItem(SAVE_KEY, JSON.stringify(formData)));
 
 function populateTextarea() {
   const saveMsg = localStorage.getItem(SAVE_KEY);
